4d1c46ecbe6c4883365ab6c3cd975c3c9466227b
2033329616 <2033329616@qq.com>
2018-04-05 00:48:44 +0800

    the convolution program in notebooks

--- 卷积神经网络/convolution.py
+++ 卷积神经网络/convolution.py
@@ -59,21 +59,22 @@ y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 
 # 训练和评估
-cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y_conv),reduction_indices=[1]))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
 
 sess = tf.InteractiveSession()
-sess.run(tf.initialize_all_variables())
+tf.global_variables_initializer().run()
+
 
 for i in range(2000):
 	batch = mnist.train.next_batch(50)
 	if i%100 ==0:
 		train_accuracy = accuracy.eval(feed_dict={x:batch[0],y_:batch[1], keep_prob:1.0})
-		print('step%d, training accuracy %g' %(i, train_accuracy))                  #%g根据实际情况输出
+		print('step%i, training accuracy %g' %(i, train_accuracy))                  #%g根据实际情况输出
 	train_step.run(feed_dict={x:batch[0], y_:batch[1], keep_prob:0.5})
-print('test accuracy %g'% accuracy.eval(feed_dict={x:mnist.test.next_batch(2000)[0], y_:mnist.test.next_batch(2000)[1], keep_prob:1.0}))
+print('test accuracy %g'% accuracy.eval(feed_dict={x:mnist.test.next_batch(200)[0], y_:mnist.test.next_batch(200)[1], keep_prob:1.0}))
 
 
 
